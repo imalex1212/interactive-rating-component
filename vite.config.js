bc3a432bf73acaed288b46504a7dc5151a066455
imalex1212 <hoangalex2404@gmail.com>
2025-09-18 21:12:52 +1000

    Ready to deploy

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/interactive-rating-component/",
   plugins: [react()],
 });
